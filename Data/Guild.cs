98610849575f1ec98243e06aa3adeb26e1a98881
Tristan Zander <knavetnz@gmail.com>
2021-07-24 12:23:43 -0400

    project: update all dependencies

--- Data/Guild.cs
+++ Data/Guild.cs
@@ -8,8 +8,9 @@ namespace Data
     {
         [Key]
         public ulong GuildId { get; set; }
+        public bool AllowQuotes = true;
         public List<Quote> Quotes { get; set; } = new List<Quote>();
-        
+        public bool AllowAudio = true;
         public AudioPlayer? AudioPlayer { get; set; }
     }
 }
