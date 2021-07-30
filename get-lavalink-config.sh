81f03de4ec1b769610fc5c534fefe6e76f5e6e9a
Tristan Zander <tristannzander@gmail.com>
2021-07-30 14:52:51 -0400

    Fixing lavalink config file

--- get-lavalink-config.sh
+++ get-lavalink-config.sh
@@ -2,17 +2,22 @@
 
 set -e
 
+# TODO
+# This script currently doesn't overwrite $LAVALINK_CONFIG if it knows it's not
+# a yml file. I just don't want to add the stdin part where you ask the user to
+# overwrite that file. 
+
 function get_lavalink_conf() { 
 	curl -o ./lavalink.yml https://raw.githubusercontent.com/freyacodes/Lavalink/master/LavalinkServer/application.yml.example
-	less ./lavalink.yml
+	$EDITOR ./lavalink.yml
 }
 
 LAVALINK_CONFIG=${LAVALINK_CONFIG:="lavalink.yml"}
 
 # Check based on environment variable.
-if [[ ! -f $LAVAINK_CONFIG ]] && [[ ! ${LAVALINK_CONFIG:${#LAVALINK_CONFIG}-4} == ".yml" ]]; then
+if [[ "${LAVALINK_CONFIG:${#LAVALINK_CONFIG}-4}" == ".yml" ]] && [[ -f "$LAVALINK_CONFIG" ]]; then
+	echo "Existing lavalink configuration found."
+else
 	echo "No lavalink file found."
 	get_lavalink_conf 
-else
-	echo "Existing lavalink configuration found."
 fi
