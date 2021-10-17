4befbd70339fd14562a416c11cd7d67d2f8a842c
Tristan Zander <knavetnz@gmail.com>
2021-10-17 18:21:00 -0400

    Started initial configuration for integrated testing.

---
+++ Bot.Test/UnitTest1.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+
+namespace Bot.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            // Setup test bot.
+        }
+
+        [Test]
+        public void AdminCommands()
+        {
+            
+            
+           Assert.Pass(); 
+        }
+
+    }
+}
